101f86a0bda867bb6b903d3b54472b1533aa6b0a
parthvichare9-ship-it <parthvichare9@gmail.com>
2026-07-11 08:11:03 +0530

    dON

---
+++ src/app/models/contactAssignment.model.ts
@@ -0,0 +1,23 @@
+import { BaseModel } from '@surefy/models/base.model';
+import { Knex } from 'knex';
+
+class ContactModel extends BaseModel {
+  constructor() {
+    super('contact_assignments');
+  }
+
+  async findByAssignedId(assigned_to:string){
+    return this.query().where('assigned_to',assigned_to).first()
+  }
+
+  async findByContactId(contactId:string){
+    return this.query().where('contact_id',contactId).returning("*")
+  }
+
+  async findContactAssigned(assigned_to:string,contactId:string){
+    return this.query().where('contact_id',contactId).andWhere('assigned_to',assigned_to).first()
+  }
+
+}
+
+export default new ContactModel()
